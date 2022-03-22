982f4968c186dc331bfb87755bd80cf2e675c910
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-03-22 22:56:58 +0100

    correct pathing error to startbrowser.sh

--- JavaScript-Calendar-Project/app/startbrowser.sh
+++ JavaScript-Calendar-Project/app/startbrowser.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo xinit ~/JavaScript-Calendar-Project/App/bin/./setupchromium.sh 
+sudo xinit ~/JavaScript-Calendar-Project/App/bin/setupchromium.sh 
 # start xinit script to start chromium browser in kiosk mode manually
