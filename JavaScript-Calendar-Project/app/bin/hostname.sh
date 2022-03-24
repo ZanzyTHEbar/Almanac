6546552df870390b00e66afd04e0eb197021800b
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-03-24 14:25:21 +0100

    add reboot clause to hostname script

--- JavaScript-Calendar-Project/app/bin/hostname.sh
+++ JavaScript-Calendar-Project/app/bin/hostname.sh
@@ -10,12 +10,18 @@ printf "============================================================\n"
 if test $# -eq 0
 then
     printf "${BOLD}${UL}${RED} ERROR - ${NC}${BOLD}${STRONG_YELLOW}Please pass-in an ideal name${NC}"
+    printf "${BOLD}${UL}${BLUE} NOTE - ${NC}${BOLD}${STRONG_YELLOW}Arguments are: ${NC}\n"
+    printf "${BOLD}${UL}${BLUE} <ideal name> | --reboot | -r | --reboot-device | -R${NC}\n"
+    printf "${BOLD}${UL}${BLUE} NOTE - ${NC}${BOLD}${STRONG_YELLOW}If no secondary argument is passed, script will not reboot the device - a manual reboot will be required${NC}\n"
     exit 1
 fi
 printf "============================================================\n"
 echo $1
 NAME=$1
+echo $2
+reboot_state=$2
 echo $NAME
+echo $reboot_state
 
 echo $NAME | sudo tee  /etc/hostname
 
@@ -27,8 +33,20 @@ sudo service hostname.sh start
 
 printf "${BOLD}${STRONG_YELLOW}Hostname set.${NC}"
 printf "${BOLD}${STRONG_YELLOW}You can use the command line to set the hostname at anytime using the following command:${NC}"
-printf "${BOLD}${STRONG_BLUE}${UL}sudo hostname.sh <hostname>${NC}"
-printf "${BOLD}${STRONG_YELLOW}Rebooting machine in 15 seconds${NC}"
-sleep 15
-sudo reboot
+printf "${BOLD}${STRONG_BLUE}${UL}sudo hostname.sh <ideal hostname> <optional: --reboot | -r | --reboot-device | -R>${NC}"
+
+case $2 in
+
+--reboot | -r | --reboot-device | -R)
+    printf "${BOLD}${UL}${RED} Warning - ${NC}${BOLD}${STRONG_YELLOW}Rebooting device in 15 seconds${NC}"
+    sleep 15
+    sudo reboot
+    ;;
+
+*)
+    printf "${BOLD}${UL}${RED} Warning - ${NC}${BOLD}${STRONG_YELLOW}Device will not reboot after setup, please manually reboot when prompted or when setup completes${NC}"
+    exit 1
+    ;;
+esac
+
 printf "============================================================\n"
