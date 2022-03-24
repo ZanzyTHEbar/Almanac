8ee04fe73e9ef48207bb07208e0a24b292417399
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-03-24 01:41:08 +0100

    ANSI colours example

---
+++ ansicolours.bat
@@ -0,0 +1,54 @@
+@echo off
+cls
+echo [101;93m STYLES [0m
+echo [0m [0mReset[0m
+echo [1m [1mBold[0m
+echo [4m [4mUnderline[0m
+echo [7m [7mInverse[0m
+echo.
+echo [101;93m NORMAL FOREGROUND COLORS [0m
+echo [30m [30mBlack[0m (black)
+echo [31m [31mRed[0m
+echo [32m [32mGreen[0m
+echo [33m [33mYellow[0m
+echo [34m [34mBlue[0m
+echo [35m [35mMagenta[0m
+echo [36m [36mCyan[0m
+echo [37m [37mWhite[0m
+echo.
+echo [101;93m NORMAL BACKGROUND COLORS [0m
+echo [40m [40mBlack[0m
+echo [41m [41mRed[0m
+echo [42m [42mGreen[0m
+echo [43m [43mYellow[0m
+echo [44m [44mBlue[0m
+echo [45m [45mMagenta[0m
+echo [46m [46mCyan[0m
+echo [47m [47mWhite[0m (white)
+echo.
+echo [101;93m STRONG FOREGROUND COLORS [0m
+echo [90m [90mWhite[0m
+echo [91m [91mRed[0m
+echo [92m [92mGreen[0m
+echo [93m [93mYellow[0m
+echo [94m [94mBlue[0m
+echo [95m [95mMagenta[0m
+echo [96m [96mCyan[0m
+echo [97m [97mWhite[0m
+echo.
+echo [101;93m STRONG BACKGROUND COLORS [0m
+echo [100m [100mBlack[0m
+echo [101m [101mRed[0m
+echo [102m [102mGreen[0m
+echo [103m [103mYellow[0m
+echo [104m [104mBlue[0m
+echo [105m [105mMagenta[0m
+echo [106m [106mCyan[0m
+echo [107m [107mWhite[0m
+echo.
+echo [101;93m COMBINATIONS [0m
+echo [31m                     [31mred foreground color[0m
+echo [7m                      [7minverse foreground ^<-^> background[0m
+echo [7;31m                   [7;31minverse red foreground color[0m
+echo [7m and nested [31m [7mbefore [31mnested[0m
+echo [31m and nested [7m [31mbefore [7mnested[0m
