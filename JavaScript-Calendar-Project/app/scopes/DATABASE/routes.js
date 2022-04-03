6eab6f92fde5cd9cdd6de2754e9f8783f237cd3b
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-04-03 20:03:28 +0200

    update database routes

--- JavaScript-Calendar-Project/app/scopes/DATABASE/routes.js
+++ JavaScript-Calendar-Project/app/scopes/DATABASE/routes.js
@@ -1,64 +1,65 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const path = require("path");
 
-router.use(express.static(path.join(__dirname, "frontend")));
-
+/* GET home page. */
 router.get(
   "/",
-  (req, res, next) => {
-    res.redirect(__dirname + "index.html");
+  function (req, res, next) {
     if (req.query) next();
   },
   (req, res, next) => {
-    res.send("Requested: " + req.query);
+    if (!req.session.userId) {
+      // Redirect unauthenticated requests to home page
+      res.redirect("/");
+    } else {
+      res.render("database");
+      //res.send("Requested: " + req.query);
+    }
     next();
   },
   (err, req, res) => {}
 );
 
-router.post(
-  "/",
-  (req, res, next) => {
-    res.send("Creating element in database");
-    next();
-  },
-  (err, req, res) => {}
-);
-
-router.put(
-  "/",
-  (req, res, next) => {
-    res.send("Update element in database");
-    next();
-  },
-  (err, req, res) => {}
-);
-
-router.delete(
-  "/",
-  (req, res, next) => {
-    res.send("Delete element in database");
-    next();
-  },
-  (err, req, res) => {}
-);
-
-router.get(
-  "/log",
-  (req, res, next) => {
-    res.redirect(__dirname + "index.html");
+router.all(
+  "/secret",
+  function (req, res, next) {
     if (req.query) next();
   },
-  (err, req, res, next) => {
-    res.send("Retrieving log");
+  (req, res, next) => {
+    if (!req.session.userId) {
+      // Redirect unauthenticated requests to home page
+      res.redirect("/");
+    } else {
+      console.log(
+        "Accessing the secret section. In this section you can access all HTTP methods."
+      );
+      switch (req.query.action) {
+        case "get":
+          console.log("Retrieving log ...");
+          res.send("Requested: " + req.query);
+          break;
+        case "post":
+          console.log("Creating element in database...");
+          res.send("Requested: " + req.query);
+          break;
+        case "put":
+          console.log("Update element in database ...");
+          res.send("Requested: " + req.query);
+          break;
+        case "delete":
+          console.log("Delete element in database ...");
+          res.send("Requested: " + req.query);
+          break;
+        default:
+          console.log("New log entry ...");
+          res.send("Requested: " + req.query);
+          break;
+      }
+      //res.render("database");
+    }
     next();
   },
   (err, req, res) => {}
 );
-
-router.post("/log", (req, res) => {
-  res.send("New log entry");
-});
 
 module.exports = router;
