61f64a72d688b51a22376ccfa2eb3d8815f1df02
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-04-10 15:28:22 +0200

    update client.js

--- JavaScript-Calendar-Project/app/public/client.js
+++ JavaScript-Calendar-Project/app/public/client.js
@@ -30,18 +30,18 @@ const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
 
 // Lights
-const pointLight = new THREE.PointLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffffff, 1, 100);
 pointLight.position.set(5, 5, 5);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(pointLight, ambientLight);
 
 // Helpers
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper)
+//const lightHelper = new THREE.PointLightHelper(pointLight)
+//const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper)
 
-// const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry_2 = new THREE.SphereGeometry(0.25, 24, 24);
 const material_2 = new THREE.MeshStandardMaterial({ color: 0xffffff });
@@ -119,7 +119,7 @@ function animate() {
 
   moon.rotation.x += 0.005;
 
-  // controls.update();
+  //controls.update();
 
   renderer.render(scene, camera);
 }
