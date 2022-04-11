0129e39f6cfd03b2dc6f22de48e7b31e17e8bf42
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-04-11 22:18:22 +0200

    update actions.js

--- JavaScript-Calendar-Project/app/server/scopes/utils/actions.js
+++ JavaScript-Calendar-Project/app/server/scopes/utils/actions.js
@@ -1,6 +1,7 @@
 const config = require("./config");
-const configJSON = require("./config.json");
+const configJSON = require("../../config.json");
 const fs = require("fs");
+const logLevel = require("./logLevels");
 const time = new Date()
   .toISOString()
   .slice(0, -8)
@@ -8,10 +9,62 @@ const time = new Date()
   .replace(/T/g, "-")
   .replace(/:/g, ".");
 const logFilePath = `${configJSON.eLog.filePath}eLog-${time}.log`;
-console.log(logFilePath);
+
+let LOGLEVEL = configJSON.eLog.level;
+let CLOG = configJSON.eLog.cLogEnabled;
+let DLOG = configJSON.eLog.dLogEnabled;
+let ELOG = configJSON.eLog.eLogEnabled;
+let FLOG = configJSON.eLog.fLogEnabled;
+let DBENABLED = configJSON.scopes.DATABASE;
+let DEVENV = config.NODE_ENV === "development";
+
+const STYLE = {
+  // Colors
+  RED: "\x1b[31m",
+  GREEN: "\x1b[32m",
+  YELLOW: "\x1b[33m",
+  BLUE: "\x1b[34m",
+  PURPLE: "\x1b[35m",
+  CYAN: "\x1b[36m",
+  WHITE: "\x1b[37m",
+  // Fonts
+  BOLD: "\x1b[1m",
+  UNDERLINE: "\x1b[4m",
+  REVERSED: "\x1b[7m",
+  // END
+  END: "\x1b[0m",
+};
 
 module.exports = {
-  utils: function (str) {
+  utilInit: () => {
+    eLog2(logLevel.INFO, "UTIL", "Initializing!");
+    eLog2(logLevel.INFO, "UTIL", "Log level is set to: " + LOGLEVEL);
+    if (DEVENV) {
+      eLog2(
+        logLevel.WARN,
+        "UTIL",
+        "Environment is set to development, log level will be overwritten"
+      );
+    }
+    if (ELOG) {
+      eLog2(
+        logLevel.INFO,
+        "UTIL",
+        "Custom extending logging 'eLog2' is enabled"
+      );
+    }
+    if (CLOG) {
+      eLog2(logLevel.INFO, "UTIL", "Console logging is enabled");
+    }
+    if (DLOG) {
+      eLog2(logLevel.INFO, "UTIL", "Database logging is enabled");
+    }
+    if (FLOG) {
+      eLog2(logLevel.INFO, "UTIL", "File logging is enabled");
+      eLog2(logLevel.INFO, "UTIL", "Log-file is saved in: " + logFilePath);
+    }
+  },
+  checkJson: (str) => {
     try {
       JSON.parse(str);
     } catch (e) {
@@ -23,115 +76,76 @@ module.exports = {
     }
     return true;
   },
-  eLog: function (rawmsg) {
-    let msg;
-    switch (rawmsg.split(" ")[0].slice(1, -1)) {
-      case "ERROR":
-        msg = "\x1b[31m" + rawmsg + "\x1b[0m";
-        break;
-      case "WARN":
-        msg = "\x1b[33m" + rawmsg + "\x1b[0m";
-        break;
-      case "FINE":
-        msg = "\x1b[32m" + rawmsg + "\x1b[0m";
-        break;
-      case "STATUS":
-        msg = "\x1b[34m" + rawmsg + "\x1b[0m";
-        break;
-      case "DEBUG":
-        msg = "\x1b[35m" + rawmsg + "\x1b[0m";
-        break;
-      default:
-        msg = "\x1b[37m" + rawmsg + "\x1b[0m";
-    }
-    if (configJSON.eLog.eLogEnabled) {
-      let cLog = configJSON.eLog.cLogEnabled || process.env.ENV === "dev";
-      if (configJSON.eLog.fLogEnabled) {
-        try {
-          if (!fs.existsSync(logFilePath)) {
-            fs.writeFileSync(
-              logFilePath,
-              "===eLog Message Files - Consider Using the included SQLite database logging===\n",
-              "utf8"
-            );
-          }
-        } catch (err) {
-          console.log("\x1b[31m[ERROR] [UTIL] Error creating eLog file\x1b[0m");
+  disableLogBase: () => {
+    eLog(logLevel.WARN, "UTIL", "Disabling logging database");
+    DBENABLED = false;
+  },
+  eLog: (level, scope, rawmsg, forceConsole = false) => {
+    eLog2(level, scope, rawmsg, forceConsole);
+  },
+  style: STYLE,
+};
+
+function eLog2(level, scope, rawmsg, forceConsole = false) {
+  if (level.value < LOGLEVEL) return;
+  let msg = getMSG(level, scope, rawmsg);
+
+  if (ELOG) {
+    let cLog = CLOG || DEVENV;
+    if (FLOG) {
+      try {
+        if (!fs.existsSync(logFilePath)) {
+          fs.writeFileSync(
+            logFilePath,
+            "===eLog Message Files - Consider Using the included SQLite database logging===\n",
+            "utf8"
+          );
         }
-        fs.appendFileSync(logFilePath, `${rawmsg}\n`, "utf8");
-      }
-      if (configJSON.eLog.dLogEnabled && configJSON.scopes.DATABASE) {
-        const db = require("../DATABASE/actions");
-        db.logMessage(rawmsg.split(" "));
-      } else if (configJSON.eLog.dLogEnabled) {
+      } catch (err) {
         console.log(
-          `"\x1b[33m[UTIL] eLog (DATABASE) is enabled but scope DATABASE is not\x1b[0m`
+          `${STYLE.RED}[ERROR] [UTIL] Error creating eLog file${STYLE.END}`
         );
-        cLog = true;
-      } else if (configJSON.scopes.DATABASE) {
-        console.log(
-          `"\x1b[33m[UTIL] scope DATABASE is enabled, consider using eLog (DATABASE)\x1b[0m`
-        );
-        cLog = true;
       }
-      if (cLog) {
-        console.log(msg);
-      }
-    } else {
+      fs.appendFileSync(logFilePath, `${msg.slice(5, -7)}\n`, "utf8");
+    }
+    if (DLOG && DBENABLED) {
+      const db = require("../DATABASE/actions");
+      db.logMessage(msg.slice(28, -7));
+    } else if (DLOG) {
+      console.log(
+        `${STYLE.YELLOW}[UTIL] eLog (DATABASE) is enabled but scope DATABASE is not${STYLE.END}`
+      );
+      cLog = true;
+    } else if (DBENABLED) {
+      console.log(
+        `${STYLE.YELLOW}[UTIL] scope DATABASE is enabled, consider using eLog (DATABASE)${STYLE.END}`
+      );
+      cLog = true;
+    }
+    if (cLog || forceConsole) {
       console.log(msg);
     }
+  } else {
+    console.log(msg);
+  }
+}
 
-    // if(configJSON.eLog.eLogEnabled) {
-    //     switch(true) {
-    //         case configJSON.eLog.cLogEnabled || process.env.ENV === 'dev':
-    //             console.log(msg);
-    //         case configJSON.eLog.fLogEnabled:
-    //             try {
-    //                 if (!fs.existsSync(logFilePath)) {
-    //                     fs.writeFileSync(logFilePath, "===eLog Message Files - Consider Using the included SQLite database logging===\n", "utf8");
-    //                 }
-    //             } catch (err) {
-    //             console.log("[UTIL] Error creating eLog file");
-    //             }
-    //             fs.appendFileSync(logFilePath, `${msg}\n`, "utf8");
-    //         case (configJSON.eLog.dLogEnabled && configJSON.scopes.DATABASE && process.env.DATABASE_ENABLED):
-    //             const db = require('../DATABASE/actions');
-    //             db.logMessage(msg);
-    //             break;
-    //         // WHY IS THERE NO XOR OPERATOR ARGH
-    //         case configJSON.eLog.dLogEnabled && ((configJSON.scopes.DATABASE && !process.env.DATABASE_ENABLED) || (!configJSON.scopes.DATABASE && process.env.DATABASE_ENABLED)):
-    //             console.log(`[UTIL] eLog (DATABASE) is enabled but scope DATABASE is not.`);
-    //             console.log(msg);
-    //             break;
-    //         case !configJSON.eLog.dLogEnabled && !(process.env.DATABASE_ENABLED && configJSON.scopes.DATABASE):
-    //             console.log(`[UTIL] scope DATABSE is enabled, consider using eLog (DATABASE).`);
-    //             console.log(msg);
-    //             break;
-    //         default:
-    //             console.log(msg);
-    //     }
-    // } else {
-    //     console.log(msg);
-    // }
-
-    // // if (configJSON.eLog.enabled && configJSON.scopes.DATABASE && configJSON.eLog.dLogEnabled) {
-    // //     const db = require('../DATABASE/actions');
-    // //     db.logMessage(msg);
-    // // } else if (configJSON.eLog.enabled && configJSON.eLog.fLogEnabled) {
-    // //     try {
-    // //         if (!fs.existsSync(logFilePath)) {
-    // //             fs.writeFileSync(logFilePath, "===eLog Message Files - Consider Using the included SQLite database logging===\n", "utf8");
-    // //         }
-    // //     } catch (err) {
-    // //       console.log("[UTIL] Error creating eLog file");
-    // //     }
-    // //     fs.appendFileSync(logFilePath, `${msg}\n`, "utf8");
-    // // } else {
-    // //     console.log(msg);
-    // //     cLog = false;
-    // // }
-    // // if (cLog || configJSON.eLog.cLogEnabled || process.env.ENV == "DEV") {
-    // //     console.log(msg);
-    // // }
-  },
-};
+function getMSG(level, scope, rawmsg) {
+  let logTime = new Date().toISOString().replace(/T/g, " ").slice(0, -1);
+  switch (level) {
+    case logLevel.ERROR:
+      return `${STYLE.RED}${logTime} [${level.def}] [${scope}] ${rawmsg}${STYLE.END}`;
+    case logLevel.WARN:
+      return `${STYLE.YELLOW}${logTime} [${level.def}] [${scope}] ${rawmsg}${STYLE.END}`;
+    case logLevel.STATUS:
+      return `${STYLE.BLUE}${logTime} [${level.def}] [${scope}] ${rawmsg}${STYLE.END}`;
+    case logLevel.INFO:
+      return `${STYLE.WHITE}${logTime} [${level.def}] [${scope}] ${rawmsg}${STYLE.END}`;
+    case logLevel.FINE:
+      return `${STYLE.GREEN}${logTime} [${level.def}] [${scope}] ${rawmsg}${STYLE.END}`;
+    case logLevel.DEBUG:
+      return `${STYLE.PURPLE}${logTime} [${level.def}] [${scope}] ${rawmsg}${STYLE.END}`;
+    default:
+      return `${STYLE.CYAN}${logTime} [${level.def}] [${scope}] ${rawmsg} (UNSUPPORTED LEVEL)${STYLE.END}`;
+  }
+}
