d3d9413ba90473647742fb8b91c5c00048564aae
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-04-13 02:35:33 +0200

    update

--- JavaScript-Calendar-Project/app/server/bin/setup.py
+++ JavaScript-Calendar-Project/app/server/bin/setup.py
@@ -1,8 +1,12 @@
 # MUST SUDO FOR THIS TO WORK - ELSE PERMISSION DENIED
 
+import json
 import subprocess as s
+import os
+from pathlib import Path
 from time import sleep as zzz
 from colorama import Fore, Style
+
 
 # print ascii art logo
 def welcome():
@@ -17,9 +21,94 @@ def welcome():
 |_|   |_____\_____\__,_|_|\___|_| |_|\__,_|\__,_|_|   """)
     print(f" {Style.RESET_ALL}")
     print(f"{Fore.BLUE}Welcome to the setup script for the OutLook Knight Project{Style.RESET_ALL}")
-    print(f"{Fore.YELLOW}This script will install all the necessary packages and setup the project to run{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}This script will install all the necessary packages and setup the project to run{Style.RESET_ALL}")
     zzz(5)
+
+
+def run_fast_scandir(dir):    # dir: str, ext: list
+    subfolders = []
+
+    for f in os.scandir(dir):
+        if f.is_dir():
+            subfolders.append(f.path)
+
+    for dir in list(subfolders):
+        sf = run_fast_scandir(dir)
+        subfolders.extend(sf)
+    return subfolders
+
+
+def createConfigJSONFile():
+    # use user input to dynamically create config.json file
+    # with the following settings:
+    #   - description
+    #   - scopes {Database, DLNA, MSCAL, GOOGLECAL}
+    #   - elog {level, cLogEnabled, dLogEnabled, eLogEnabled, fLogEnabled, utilPath, filePath}
+    temp = {
+        "scopes": {
+            "Database": False,
+            "DLNA": False,
+            "MSCAL": False,
+            "GOOGLECAL": False
+        },
+        "elog": {
+            "level": 0,
+            "cLogEnabled": False,
+            "dLogEnabled": False,
+            "eLogEnabled": False,
+            "fLogEnabled": False,
+            "utilPath": "",
+            "filePath": ""
+        }
+    }
+
+    configPath = Path(__file__).parent
+
+    relative_path = '../server/scopes/utils'
+    relative_path_2 = '../../app'
+    relative_path_3 = '../server/scopes/'
+
+    src_path = (configPath / relative_path).resolve()
+    src_path_2 = (configPath / relative_path_2).resolve()
+    src_path_3 = (configPath / relative_path_3).resolve()
+
+    temp["elog"]["utilPath"] = str(src_path)
+    temp["elog"]["filePath"] = str(src_path_2 / "Logs/")
+
+    completeName = str(src_path / "config.json")
+
+    subfolders = run_fast_scandir(src_path_3)
     
+    print(f"{Fore.GREEN}The following subfolders were found in the scopes folder{Style.RESET_ALL}")
+    for folder in subfolders:
+        print(f"{Fore.GREEN}{folder}{Style.RESET_ALL}")
+
+    print(f"{Fore.BLUE}Please enter the following information to create the config.json file{Style.RESET_ALL}")
+    print(f"{Fore.BLUE}Please choose the modules to enable (0 or 1 only).{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable Database Module?{Style.RESET_ALL}")
+    temp["scopes"]["Database"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable DLNA Client Module?{Style.RESET_ALL}")
+    temp["scopes"]["DLNA"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable Microsoft Calendar Module?{Style.RESET_ALL}")
+    temp["scopes"]["MSCAL"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable Google Calendar Module?{Style.RESET_ALL}")
+    temp["scopes"]["GOOGLECAL"] = bool(int(input()))
+    print(f"{Fore.BLUE}Please enter the elog level (a number between 0 and 10){Style.RESET_ALL}")
+    temp["elog"]["level"] = int(input())
+    print(f"{Fore.BLUE}Please choose the logging modules to enable (0 or 1 only).{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable the elog cLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["cLogEnabled"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable the elog dLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["dLogEnabled"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable the elog eLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["eLogEnabled"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable the elog fLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["fLogEnabled"] = bool(int(input()))
+
+    with open(completeName, "w") as outfile:
+        json.dump(temp, outfile)
+
+
 def setupServer():
     chmod_setup = "sudo chmod +x setup.sh".split()
     s.run(
@@ -54,9 +143,9 @@ def rotateScreen():
     print(f"{Fore.YELLOW}Would you like to rotate the screen? (y/n)\n")
     rotate_screen = input()
     if rotate_screen == "y" or rotate_screen == "Y":
-        print(f"How many degrees? (0-3), 0 is to reset and 3 is 270°. Please use a space to separate answers{Style.RESET_ALL}.\n")
+        print(
+            f"How many degrees? (0-3), 0 is to reset and 3 is 270°. Please use a space to separate answers{Style.RESET_ALL}.\n")
         degrees = input()
-        print(f"{Fore.BLUE}Your choice to rotate screen{Style.RESET_ALL}", rotate_screen)
         print(f"{Fore.GREEN}Your degrees setting{Style.RESET_ALL}", degrees)
         print(f"{Fore.BLUE}Continuing with setup, setting up rotation{Style.RESET_ALL}")
         rotation_settings = getRotationSettings(degrees)
@@ -122,6 +211,7 @@ def setupKiosk():
 
 if __name__ == '__main__':
     welcome()
+    createConfigJSONFile()
     checkPackages()
     setupKiosk()
     rotateScreen()
--- JavaScript-Calendar-Project/extras/test.py
+++ JavaScript-Calendar-Project/extras/test.py
@@ -1,7 +1,14 @@
+import json
+import re
+import subprocess as s
+import os
+from pathlib import Path
 from time import sleep as zzz
 from colorama import Fore, Style
 
 # print ascii art logo
+
+
 def welcome():
     print(f" {Fore.MAGENTA}")
     print(r"""
@@ -14,9 +21,104 @@ def welcome():
 |_|   |_____\_____\__,_|_|\___|_| |_|\__,_|\__,_|_|   """)
     print(f" {Style.RESET_ALL}")
     print(f"{Fore.BLUE}Welcome to the setup script for the OutLook Knight Project{Style.RESET_ALL}")
-    print(f"{Fore.YELLOW}This script will install all the necessary packages and setup the project to run{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}This script will install all the necessary packages and setup the project to run{Style.RESET_ALL}")
     zzz(5)
-    
+
+
+def scandir(path):    # dir: str, ext: list
+    subfolders = []
+
+    for f in os.scandir(path):
+        if f.is_dir():
+            subfolders.append(f.path)
+
+    """ for path in list(subfolders):
+        subfol = scandir(path)
+        subfolders.extend(subfol) """
+    return subfolders
+
+
+def createConfigJSONFile():
+    # use user input to dynamically create config.json file
+    # with the following settings:
+    #   - description
+    #   - scopes {Database, DLNA, MSCAL, GOOGLECAL}
+    #   - elog {level, cLogEnabled, dLogEnabled, eLogEnabled, fLogEnabled, utilPath, filePath}
+    temp = {
+        "scopes": {
+            "Database": False,
+            "DLNA": False,
+            "MSCAL": False,
+            "GOOGLECAL": False
+        },
+        "elog": {
+            "level": 0,
+            "cLogEnabled": False,
+            "dLogEnabled": False,
+            "eLogEnabled": False,
+            "fLogEnabled": False,
+            "utilPath": "",
+            "filePath": ""
+        }
+    }
+
+    configPath = Path(__file__).parent
+
+    relative_path = '../app/server/scopes/utils'
+    relative_path_2 = '../app'
+    relative_path_3 = '../app/server/scopes/'
+
+    src_path = (configPath / relative_path).resolve()
+    src_path_2 = (configPath / relative_path_2).resolve()
+    src_path_3 = (configPath / relative_path_3).resolve()
+
+    temp["elog"]["utilPath"] = str(src_path)
+    temp["elog"]["filePath"] = str(src_path_2 / "Logs/")
+
+    completeName = str(src_path / "config.json")
+
+    subfolders = scandir(src_path_3)
+
+    # print(f"{Fore.GREEN}The following subfolders were found in the scopes folder{Style.RESET_ALL}")
+    for folder in subfolders:
+        temp = []
+        temp.append(folder.split("scopes\\")[-1])
+        # print(f"{Fore.GREEN}{temp}{Style.RESET_ALL}")
+        for i in list(temp):
+            temp = i.split("\\")[-1]
+            # check if temp_2 has lowercase letters
+            if any(u.isupper() for u in list(temp)):
+                temp = temp
+                # print the 3rd element of the list
+                print(f"{Fore.GREEN}{temp}{Style.RESET_ALL}")
+
+    """ print(f"{Fore.BLUE}Please enter the following information to create the config.json file{Style.RESET_ALL}")
+    print(f"{Fore.BLUE}Please choose the modules to enable (0 or 1 only).{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable Database Module?{Style.RESET_ALL}")
+    temp["scopes"]["Database"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable DLNA Client Module?{Style.RESET_ALL}")
+    temp["scopes"]["DLNA"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable Microsoft Calendar Module?{Style.RESET_ALL}")
+    temp["scopes"]["MSCAL"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable Google Calendar Module?{Style.RESET_ALL}")
+    temp["scopes"]["GOOGLECAL"] = bool(int(input()))
+    print(f"{Fore.BLUE}Please enter the elog level (a number between 0 and 10){Style.RESET_ALL}")
+    temp["elog"]["level"] = int(input())
+    print(f"{Fore.BLUE}Please choose the logging modules to enable (0 or 1 only).{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable the elog cLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["cLogEnabled"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable the elog dLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["dLogEnabled"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable the elog eLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["eLogEnabled"] = bool(int(input()))
+    print(f"{Fore.GREEN}Enable the elog fLogEnabled module?{Style.RESET_ALL}")
+    temp["elog"]["fLogEnabled"] = bool(int(input()))
+
+    with open(completeName, "w") as outfile:
+        json.dump(temp, outfile) """
+
+
 if __name__ == '__main__':
     welcome()
+    createConfigJSONFile()
     quit()
