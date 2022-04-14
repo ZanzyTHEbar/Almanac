dc76b540eb77ee190857b0a39c208b393d24f282
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-04-14 17:07:54 +0200

    update scripts

--- JavaScript-Calendar-Project/app/server/bin/setup.py
+++ JavaScript-Calendar-Project/app/server/bin/setup.py
@@ -8,7 +8,6 @@ from time import sleep as zzz
 from colorama import Fore, Style
 
 
-# print ascii art logo
 def welcome():
     print(f"{Fore.MAGENTA} ")
     print(r"""
@@ -24,7 +23,7 @@ def welcome():
     zzz(5)
 
 
-def scandir(path):
+def scandir(path):    # dir: str, ext: list
     subfolders = []
 
     for f in os.scandir(path):
@@ -42,11 +41,11 @@ def createConfigJSONFile():
     # with the following settings:
     #   - description
     #   - scopes {Database, DLNA, MSCAL, GOOGLECAL}
-    #   - elog {level, cLogEnabled, dLogEnabled, eLogEnabled, fLogEnabled, utilPath, filePath}
+    #   - eLog {level, cLogEnabled, dLogEnabled, eLogEnabled, fLogEnabled, utilPath, filePath}
     json_file = {
         "scopes": {
         },
-        "elog": {
+        "eLog": {
             "level": 0,
             "cLogEnabled": False,
             "dLogEnabled": False,
@@ -59,41 +58,36 @@ def createConfigJSONFile():
 
     configPath = Path(__file__).parent
 
-    relative_path = '../server/scopes/utils'
-
-    relative_path_2 = '../Logs'
-
-    relative_path_3 = '../server/scopes/'
+    relative_path = '../scopes/utils'
+    relative_path_2 = '../scopes/'
+    relative_path_3 = '../../logs/'
 
     src_path = (configPath / relative_path).resolve()
-
     src_path_2 = (configPath / relative_path_2).resolve()
-
     src_path_3 = (configPath / relative_path_3).resolve()
 
-    json_file["elog"]["utilPath"] = str(src_path)
-
-    json_file["elog"]["filePath"] = str(src_path_2)
+    json_file["eLog"]["utilPath"] = str(src_path)
+    json_file["eLog"]["filePath"] = str(src_path_3)
 
     completeName = str(src_path / "config.json")
 
-    subfolders = scandir(src_path_3)
+    subfolders = scandir(src_path_2)
 
+    # print(f"{Fore.GREEN}The following subfolders were found in the scopes folder{Style.RESET_ALL}")
     print(
         f"{Fore.BLUE}Please enter the following information to create the config.json file{Style.RESET_ALL}")
+
     print(
         f"{Fore.BLUE}Please choose the modules to enable (0 or 1 only).{Style.RESET_ALL}")
 
     for folder in subfolders:
         temp = []
-
         temp.append(folder.split("scopes\\")[-1])
-
+        # print(f"{Fore.GREEN}{temp}{Style.RESET_ALL}")
         for i in list(temp):
             temp = i.split("\\")[-1]
             # check if temp_2 has lowercase letters
             if any(u.isupper() for u in list(temp)):
-
                 temp = temp
 
                 print(f"{Fore.GREEN}Found the scope: {Style.RESET_ALL}")
@@ -104,27 +98,27 @@ def createConfigJSONFile():
 
                 json_file["scopes"][temp] = bool(int(input()))
 
-    print(f"{Fore.BLUE}Please enter the elog level (a number between 0 and 10){Style.RESET_ALL}")
+    print(f"{Fore.BLUE}Please enter the eLog level (a number between 0 and 10){Style.RESET_ALL}")
 
-    json_file["elog"]["level"] = int(input())
+    json_file["eLog"]["level"] = int(input())
 
     print(f"{Fore.BLUE}Please choose the logging modules to enable (0 or 1 only).{Style.RESET_ALL}")
 
-    print(f"{Fore.GREEN}Enable the elog cLogEnabled module?{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable the eLog cLogEnabled module?{Style.RESET_ALL}")
 
-    json_file["elog"]["cLogEnabled"] = bool(int(input()))
+    json_file["eLog"]["cLogEnabled"] = bool(int(input()))
 
-    print(f"{Fore.GREEN}Enable the elog dLogEnabled module?{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable the eLog dLogEnabled module?{Style.RESET_ALL}")
 
-    json_file["elog"]["dLogEnabled"] = bool(int(input()))
+    json_file["eLog"]["dLogEnabled"] = bool(int(input()))
 
-    print(f"{Fore.GREEN}Enable the elog eLogEnabled module?{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable the eLog eLogEnabled module?{Style.RESET_ALL}")
 
-    json_file["elog"]["eLogEnabled"] = bool(int(input()))
+    json_file["eLog"]["eLogEnabled"] = bool(int(input()))
 
-    print(f"{Fore.GREEN}Enable the elog fLogEnabled module?{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Enable the eLog fLogEnabled module?{Style.RESET_ALL}")
 
-    json_file["elog"]["fLogEnabled"] = bool(int(input()))
+    json_file["eLog"]["fLogEnabled"] = bool(int(input()))
 
     with open(completeName, "w") as outfile:
         json.dump(json_file, outfile)
