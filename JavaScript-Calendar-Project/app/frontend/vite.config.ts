fdb809b37ae060b0e6af1c5877357e1d98b7feca
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-23 21:34:10 +0100

    update - migrate to project

--- JavaScript-Calendar-Project/app/frontend/vite.config.ts
+++ JavaScript-Calendar-Project/app/frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 const path = require('path')
-/* import { ViteToml } from 'vite-plugin-toml' */
 
 
 export default defineConfig({
