637f5f680147d891811ef14d920a3d3431d67735
DaOfficialWizard <45744329+ZanzyTHEbar@users.noreply.github.com>
2023-10-06 00:17:35 +0100

    refactor: add workspace and remove old app code

--- JavaScript-Calendar-Project/app-old/server/bin/setupchromium.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo matchbox-window-manager -use_cursor no& # disable cursor and activate window manager
-sudo chromium-browser # start chromium browser
-
-# use this script to start the app in kiosk mode
--- JavaScript-Calendar-Project/app-old/startbrowser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo xinit ~/JavaScript-Calendar-Project/App/bin/setupchromium.sh 
-# start xinit script to start chromium browser in kiosk mode manually
